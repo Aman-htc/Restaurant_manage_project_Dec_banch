22512acecaf9434aa3347f047a556b5231a3f1f6
Aman kushwaha <amanhtc1112kushwaha@gmail.com>
2025-05-09 10:01:42 +0530

    Menu order program

---
+++ Project/Domain/order/order_items.py
@@ -0,0 +1,161 @@
+
+from Error_handal.logger import write_logs
+import json
+import datetime
+import time
+
+# Main class to take order
+class Restaurant_Order:
+    def __init__(self, path):
+        self.path = path
+        self.total_balance = 0
+
+        # Load menu from file
+        with open(self.path, 'r') as file:
+            self.load_menu_details = json.load(file)
+
+    # Main order method
+    def order(self):
+        while True:
+            print()
+            print('=' * 30)
+            print('1. Breakfast item order...')
+            print('2. Lunch item order...')
+            print('=' * 30)
+
+            try:
+                order_menu = input('Please select any option: ')
+                print()
+
+                if order_menu.isdigit():
+                    order_menu = int(order_menu)
+
+                    # Breakfast order
+                    if order_menu == 1:
+                        self.date = datetime.datetime.now()
+                        self.list1 = self.load_menu_details[0]
+
+                        while True:
+                            self.order_item = input('Enter order name: ')
+                            print()
+                            confirm_order = input('This order is confirm (yes/no): ')
+                            if confirm_order.lower() == "yes":
+                                print('\nOrder searching', end='')
+                                for n in range(5):
+                                    time.sleep(1)
+                                    print('.', end='')
+                                print()
+
+                                found = False
+                                for order in self.list1:
+                                    for key, value in order.items():
+                                        if value == self.order_item:
+                                            print('Order is successfully')
+                                            print('Your Order is: ', order)
+                                            self.total_balance += order['price']
+                                            self.confirm_data = {
+                                                'Item_name': self.order_item,
+                                                'datetime': str(self.date),
+                                                'Order': 'confirm'
+                                            }
+                                            self.save_order_details.append(self.confirm_data)
+                                            found = True
+                                if not found:
+                                    print('Item not available!')
+
+                            else:
+                                print('Your order is canceled!')
+                                self.cancel_data = {
+                                    'Item_name': self.order_item,
+                                    'datetime': str(self.date),
+                                    'Order': 'cancel'
+                                }
+                                self.save_order_details.append(self.cancel_data)
+
+                            ask_order = input('Order more? (yes/no): ')
+                            if ask_order.lower() != 'yes':
+                                break
+
+                    # Lunch order
+                    elif order_menu == 2:
+                        self.date = datetime.datetime.now()
+                        self.list2 = self.load_menu_details[1]
+
+                        while True:
+                            self.order_item = input('Please order item name: ')
+                            print()
+                            confirm_order = input('Confirm order (yes/no): ')
+                            print()
+                            if confirm_order.lower() == 'yes':
+                                print('Order searching', end='')
+                                for n in range(5):
+                                    time.sleep(1)
+                                    print('.', end='')
+                                print()
+
+                                found = False
+                                for order in self.list2:
+                                    for key, value in order.items():
+                                        if value == self.order_item:
+                                            print('Order is successfully')
+                                            print('Your Order is:', order)
+                                            self.total_balance += order['price']
+                                            self.confirm_data = {
+                                                'Item_name': self.order_item,
+                                                'datetime': str(self.date),
+                                                'Order': 'confirm'
+                                            }
+                                            self.save_order_details.append(self.confirm_data)
+                                            found = True
+                                if not found:
+                                    print('Item not available!')
+                            else:
+                                print('Your order is canceled!')
+                                self.cancel_data = {
+                                    'Item_name': self.order_item,
+                                    'datetime': str(self.date),
+                                    'Order': 'cancel'
+                                }
+                                self.save_order_details.append(self.cancel_data)
+
+                            ask_order = input('Order more? (yes/no): ')
+                            if ask_order.lower() != 'yes':
+                                break
+                    else:
+                        print('Enter 1 or 2 only!')
+                else:
+                    print('Invalid input. Use number!')
+
+            except Exception as e:
+                date = datetime.datetime.now()
+                error_list = {
+                    'error': str(e),
+                    'function name': "order",
+                    'class name': 'Restaurant_Order',
+                    'datetime': date
+                }
+                write_logs(str(error_list))
+                print('Technical issue. Please wait!')
+
+            # Ask to go back
+            back_to_menu = input('Back to menu (yes/no): ')
+            print()
+            if back_to_menu.lower() != 'yes':
+                break
+
+
+
+class Save_Order(Restaurant_Order):
+    # Load order history
+    def load(self, path):
+        self.order_path = path
+        try:
+            with open(self.order_path, 'r') as file:
+                self.save_order_details = json.load(file)
+        except Exception as e:
+            self.save_order_details = []
+
+    # Save order history
+    def save_details(self):
+        with open(self.order_path, 'w') as file:
+            json.dump(self.save_order_details, file, indent=4)
