e3cb69acb240071d1679ebfe9f30df16f5aa899d
Aman kushwaha <amanhtc1112kushwaha@gmail.com>
2025-05-15 12:22:51 +0530

    check all data report

--- Project/Report/check_details.py
+++ Project/Report/check_details.py
@@ -1,58 +1,87 @@
 
-# Importing json for reading and writing JSON data
+# # Importing json for reading and writing JSON data
 import json
+import datetime
 
+from Error_handal.logger import write_logs
+from All_path.path import  Load_amount_path, Save_order_path, Staff_path
 
-from All_path.path import Menu_path, Load_amount_path, Save_order_path, Staff_path, table_path
-
-# Parent class
+# # Parent class
 class Check_Staff:
+    try:
+        
 
-    def admin_data(self, path):
-        self.path = path
-        with open(self.path, 'r') as file:
-            # Load JSON data into self.data_store
-            self.data_store = json.load(file)  
+        def admin_data(self, path):
+            self.path = path
+            with open(self.path, 'r') as file:
+                # Load JSON data into self.data_store
+                self.data_store = json.load(file)  
 
-    # Method to prompt user input and check if it matches any entry in the loaded data
-    def input_data(self):
-        self.enter_name = input('please enter your data: ')
-        found = False
-        for item in self.data_store:
-            for key, value in item.items():
-                if value == self.enter_name:
-                    # Print the matching record
-                    print(item)  
-                    found = True
-        if not found:
-            print('invalid name no store this data')  # If no match found
+        
 
-    # Method to display all loaded data in formatted JSON
-    def all_display_store_data(self):
-        print(json.dumps(self.data_store, indent=4))
+        # Method to display all loaded data in formatted JSON
+        def all_display_store_data(self):
+            print(json.dumps(self.data_store, indent=4))
+    except Exception as e:
+        error_list={'error':str(e),'class_name':'Check_staff'}
+        write_logs(str(error_list))
+        print('Technical issue please wait!')
 
-
-# Subclass for handling order list, inherits from Check_Admin
-class Order_List(Check_Staff):
-    def load_order(self, path):
-        self.order_path = path
-        with open(self.order_path, 'r') as file:
-            self.data_store = json.load(file)  # Load order data
+# # Subclass for handling order
+class Order_List:
+    try:
+        
+        def __init__(self,path):
+            self.order_path = path
+            with open(self.order_path, 'r') as file:
+                self.data_store = json.load(file)  # Load order data
             
-    def confirm(self):
-        for n in self.data_store:
-            for key,value in n.items():
-                if key == "Order" and value == "confirm":
-                    print(n)
-    def cancel(self):
-        for n in self.data_store:
-            for key,value in n.items():
-                if key == "Order" and value == "cancel":
-                    print(n)            
+        def load_order(self):
+            
+            # Ask user how many past days of orders to display
+            self.num_days = int(input("Enter the number of past days to views order details?: "))
+
+            self.today = datetime.datetime.now().date()  # Current date
+            self.start_date = self.today - datetime.timedelta(days=self.num_days)  # Start date
+
+            found = False
+            for order in self.data_store:
+                order_time_str = order.get("datetime", "")
+                try:
+                    # Convert datetime string to date
+                    order_date = datetime.datetime.strptime(order_time_str, "%Y-%m-%d %H:%M:%S.%f").date()
+                except Exception as e:
+                    continue  # Skip if format is invalid
+
+                # Check if order date is within the range
+                if self.start_date <= order_date <= self.today:
+                    print(json.dumps(order,indent=4))
+                    found = True
+
+            if not found:
+                print("No orders found in this date range.")
+                
+        def confirm(self):
+            for n in self.data_store:
+                for key,value in n.items():
+                    if key == "Order" and value == "confirm":
+                        print(n)
+                        
+        def cancel(self):
+            for n in self.data_store:
+                for key,value in n.items():
+                    if key == "Order" and value == "cancel":
+                        print(n)  
+                                  
+    except Exception as e:
+        error_list={'error':str(e),'class_name':'Order_list'}
+        write_logs(str(error_list))
+        print('Technical issue please wait!')    
+        
             
 def order_data():
     print('-'*30)
-    print('1. All data check!')            
+    print('1. Order details check!!')            
     print('2. Confirm order details!')
     print('3. Cancel order details!')
     print('4. Exit!')
@@ -63,16 +92,17 @@ def order_data():
         if input_data.isdigit():
             input_data=int(input_data)
             if input_data == 1:
-                data=Order_List()
-                data.load_order(Save_order_path)
-                data. all_display_store_data()
+                data=Order_List(Save_order_path)
+                
+                data.load_order()
+                
             elif input_data == 2:
-                data=Order_List()    
-                data.load_order(Save_order_path)
+                data=Order_List(Save_order_path)    
+                
                 data.confirm()
             elif input_data == 3:
-                data= Order_List()    
-                data.load_order(Save_order_path)
+                data= Order_List(Save_order_path)    
+        
                 data.cancel()
             elif input_data == 4:
                 break    
@@ -81,30 +111,62 @@ def order_data():
             
 
 
-# Subclass for handling amount list, inherits from Check_Admin
-class Amount_list(Check_Staff):
-    def load_amount(self, path):
-        self.amount_path = path
-        with open(self.amount_path, 'r') as file:
-            self.data_store = json.load(file)  # Load amount data
-            
-    def online_check(self):
-        for n in self.data_store:
-            for key,value in n.items():
-                if key ==  'payment' and value == 'online':
-                    print(n)
-                
-    def cash_check(self):
-        for n in self.data_store:
-            for key,value in n.items():
-                if key == 'payment' and value == 'cash':
-                    print(n)                       
+ 
+class Amount_list():
+    try:
+        
+        def __init__(self, path):
+            self.amount_path = path
+            with open(self.amount_path, 'r') as file:
+                self.data_store = json.load(file)  # Load amount data
+        
 
+        def load_amount(self):
+            
+
+            # Ask user how many past days of orders to display
+            self.num_days = int(input("Enter the number of past days to views bills details: "))
+
+            self.today = datetime.datetime.now().date()  # Current date
+            self.start_date = self.today - datetime.timedelta(days=self.num_days)  # Start date
+
+            found = False
+            for order in self.data_store:
+                order_time_str = order.get("datetime", "")
+                try:
+                    # Convert datetime string to date
+                    order_date = datetime.datetime.strptime(order_time_str, "%Y-%m-%d %H:%M:%S.%f").date()
+                except Exception as e:
+                    continue  # Skip if format is invalid
+
+                # Check if order date is within the range
+                if self.start_date <= order_date <= self.today:
+                    print(json.dumps(order,indent=4))
+                    found = True
+
+            if not found:
+                print("No orders found in this date range.")
+
+        def online_check(self):
+            for n in self.data_store:
+                for key,value in n.items():
+                    if key ==  'payment' and value == 'online':
+                        print(n)
+                    
+        def cash_check(self):
+            for n in self.data_store:
+                for key,value in n.items():
+                    if key == 'payment' and value == 'cash':
+                        print(n)                       
+    except Exception as e:
+        error_list={'error':str(e),'class_name':'Amount_list'}
+        write_logs(str(error_list))
+        print('Technical issue please wait!')
         
 def amount_check():
     
     print('-'*30)
-    print('1. check all amount details!')
+    print('1. check amount details!')
     print('2. Check online payment!')
     print('3. Check cash payment!')
     print('4. Exit!')
@@ -115,16 +177,16 @@ def amount_check():
         if input_data.isdigit():
             input_data=int(input_data)
             if input_data == 1:
-                data=Amount_list()
-                data.load_amount(Load_amount_path)
-                data.all_display_store_data()
+                data=Amount_list(Load_amount_path)
+                data.load_amount()
+                
             elif input_data == 2:
-                data=Amount_list()    
-                data.load_amount(Load_amount_path)
+                data=Amount_list(Load_amount_path)    
+            
                 data.online_check()
             elif input_data == 3:
-                data=Amount_list()
-                data.load_amount(Load_amount_path)
+                data=Amount_list(Load_amount_path)
+        
                 data.cash_check()
             elif input_data == 4:
                 break    
@@ -136,13 +198,6 @@ def amount_check():
             
 
 
-# Subclass for handling table booking details, inherits from Check_Admin
-class Table_book(Check_Staff):
-    def load_table(self, path):
-        self.table_path = path
-        with open(self.table_path, 'r') as file:
-            self.data_store = json.load(file)  # Load table data
-
 
 # Main function to check various data types
 def all_data_check():
@@ -152,7 +207,7 @@ def all_data_check():
         print('1. Amount details check..')
         print('2. Order item check..')
         print('3. Staff details check...')
-        print('4. Table details check...')
+        print('4. Exit...')
         print()
         print('='*30)
 
@@ -167,29 +222,22 @@ def all_data_check():
           
         # Option 2: Load and display order data
         elif select == 2:
+            
             order_data()
             
-
         # Option 3: Load and display staff data
         elif select == 3:
             data = Check_Staff()
             data.admin_data(Staff_path)
             
             data.all_display_store_data()
-
-        # Option 4: Load and display table data
-        elif select == 4:
-            data = Table_book()
-            data.load_table(table_path)
-            data.all_display_store_data()
-
+            
         # Option 5: Exit the loop
-        elif select == 5:
+        elif select == 4:
             break
 
         # Invalid input case
         else:
             print('enter your digit number or not invalid number!')
        
-    
-                    
+       
