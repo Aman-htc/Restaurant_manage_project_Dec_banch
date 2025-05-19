4ebc18b0e8dc649046bc313a3b9e616e6ccf79db
Aman kushwaha <amanhtc1112kushwaha@gmail.com>
2025-05-19 22:39:43 +0530

    manage code update

--- Project/Domain/menu/management_menu.py
+++ Project/Domain/menu/management_menu.py
@@ -7,7 +7,7 @@ from All_path.path import Menu_path
 from Report.check_details import all_data_check
 from Error_handal.logger import write_logs
 from Domain.menu.restaurant_menu import check_menu
-
+import uuid
 class Restaurant_Menu:
     def __init__(self, path):
         self.path = path 
@@ -21,44 +21,45 @@ class Restaurant_Menu:
     def menu_details(self):
         try:
             self.menu_list = []
+            
         
 
             # Define Breakfast menu
             self.Breakfast_menu = [
-                    {'id': 1, 'type': 'full', 'name': 'Aloo Paratha', 'price': 50},
-                    {'id': 2, 'type': 'half', 'name': 'Aloo Paratha', 'price': 30},
-                    {'id': 3, 'type': 'full', 'name': 'Chana Ghugni', 'price': 40},
-                    {'id': 4, 'type': 'half', 'name': 'Chana Ghugni', 'price': 20},
-                    {'id': 5, 'type': 'full', 'name': 'Sattu Paratha', 'price': 50},
-                    {'id': 6, 'type': 'half', 'name': 'Sattu Paratha', 'price': 30},
-                    {'id': 7, 'type': '100g', 'name': 'Jalebi', 'price': 40},
-                    {'id': 8, 'type': 'half', 'name': 'Chai', 'price': 20},
-                    {'id': 9, 'type': 'full', 'name': 'Salad', 'price': 40},
-                    {'id': 10, 'type': 'half', 'name': 'Salad', 'price': 20},
-                    {'id': 11, 'type': '1ltr', 'name': 'Water Bottle', 'price': 20},
-                    {'id':12,'type': 'full','name':'Chola Bhatura','price':70},
-                    {'id':13,'type':'full','name':'Mix Sabji','price':50}
+                    {'id':uuid.uuid4().hex[:2] , 'type': 'full', 'name': 'Aloo Paratha', 'price': 50},
+                    {'id': uuid.uuid4().hex[:2] , 'type': 'half', 'name': 'Aloo Paratha', 'price': 30},
+                    {'id': uuid.uuid4().hex[:2] , 'type': 'full', 'name': 'Chana Ghugni', 'price': 40},
+                    {'id':uuid.uuid4().hex[:2] , 'type': 'half', 'name': 'Chana Ghugni', 'price': 20},
+                    {'id': uuid.uuid4().hex[:2] , 'type': 'full', 'name': 'Sattu Paratha', 'price': 50},
+                    {'id': uuid.uuid4().hex[:2] , 'type': 'half', 'name': 'Sattu Paratha', 'price': 30},
+                    {'id': uuid.uuid4().hex[:2] , 'type': '100g', 'name': 'Jalebi', 'price': 40},
+                    {'id': uuid.uuid4().hex[:2] , 'type': 'half', 'name': 'Chai', 'price': 20},
+                    {'id': uuid.uuid4().hex[:2] , 'type': 'full', 'name': 'Salad', 'price': 40},
+                    {'id': uuid.uuid4().hex[:2] , 'type': 'half', 'name': 'Salad', 'price': 20},
+                    {'id':uuid.uuid4().hex[:2] , 'type': '1ltr', 'name': 'Water Bottle', 'price': 20},
+                    {'id':uuid.uuid4().hex[:2] ,'type': 'full','name':'Chola Bhatura','price':70},
+                    {'id':uuid.uuid4().hex[:2] ,'type':'full','name':'Mix Sabji','price':50}
                 ]
 
             # Define Lunch menu
             self.Lunch_menu = [
-                    {'id': 1, 'type': 'full', 'name': 'Litti Chokha', 'price': 60},
-                    {'id': 2, 'type': 'half', 'name': 'Litti Chokha', 'price': 30},
-                    {'id': 3, 'type': 'full', 'name': 'Rice Dal', 'price': 70},
-                    {'id': 4, 'type': 'half', 'name': 'Rice Dal', 'price': 40},
-                    {'id': 5, 'type': 'full', 'name': 'Paneer Butter Masala', 'price': 200},
-                    {'id': 6, 'type': 'half', 'name': 'Paneer Butter Masala', 'price': 110},
-                    {'id': 7, 'type': 'full', 'name': 'Shahi Paneer', 'price': 180},
-                    {'id': 8, 'type': 'half', 'name': 'Shahi Paneer', 'price': 100},
-                    {'id': 9, 'type': 'full', 'name': 'Plain Rice', 'price': 50},
-                    {'id': 10, 'type': 'half', 'name': 'Roti (2 pcs)', 'price': 50},
-                    {'id': 11, 'type': 'full', 'name': 'Kadhi Chawal', 'price': 120},
-                    {'id': 12, 'type': 'half', 'name': 'Kadhi Chawal', 'price': 70},
-                    {'id': 13, 'type': 'half', 'name': 'Salad', 'price': 20},
-                    {'id': 14, 'type': '1ltr', 'name': 'Water Bottle', 'price': 20},
-                    {'id':15,'type':'full','name':'Mix Sabji','price':50},
-                    {'id':16, 'type': 'full','name':'Dahi','price':80},
-                    {'id':17, 'type': 'half','name':'Dahi','price':40}
+                    {'id': uuid.uuid4().hex[:2] , 'type': 'full', 'name': 'Litti Chokha', 'price': 60},
+                    {'id': uuid.uuid4().hex[:2] , 'type': 'half', 'name': 'Litti Chokha', 'price': 30},
+                    {'id': uuid.uuid4().hex[:2] , 'type': 'full', 'name': 'Rice Dal', 'price': 70},
+                    {'id': uuid.uuid4().hex[:2] , 'type': 'half', 'name': 'Rice Dal', 'price': 40},
+                    {'id': uuid.uuid4().hex[:2] , 'type': 'full', 'name': 'Paneer Butter Masala', 'price': 200},
+                    {'id': uuid.uuid4().hex[:2] , 'type': 'half', 'name': 'Paneer Butter Masala', 'price': 110},
+                    {'id': uuid.uuid4().hex[:2] , 'type': 'full', 'name': 'Shahi Paneer', 'price': 180},
+                    {'id': uuid.uuid4().hex[:2] , 'type': 'half', 'name': 'Shahi Paneer', 'price': 100},
+                    {'id': uuid.uuid4().hex[:2] , 'type': 'full', 'name': 'Plain Rice', 'price': 50},
+                    {'id': uuid.uuid4().hex[:2] , 'type': 'half', 'name': 'Roti (2 pcs)', 'price': 50},
+                    {'id': uuid.uuid4().hex[:2] , 'type': 'full', 'name': 'Kadhi Chawal', 'price': 120},
+                    {'id': uuid.uuid4().hex[:2] , 'type': 'half', 'name': 'Kadhi Chawal', 'price': 70},
+                    {'id': uuid.uuid4().hex[:2] , 'type': 'half', 'name': 'Salad', 'price': 20},
+                    {'id': uuid.uuid4().hex[:2] , 'type': '1ltr', 'name': 'Water Bottle', 'price': 20},
+                    {'id':uuid.uuid4().hex[:2] ,'type':'full','name':'Mix Sabji','price':50},
+                    {'id':uuid.uuid4().hex[:2] , 'type': 'full','name':'Dahi','price':80},
+                    {'id':uuid.uuid4().hex[:2] , 'type': 'half','name':'Dahi','price':40}
                 ]
 
             # Append both menus to the main list
@@ -92,6 +93,7 @@ class Manage_Restaurant:
     # function for managing menu items
     def manage_item(self):
         try:
+            self.Id_Item = uuid.uuid4().hex[:2] 
             while True:
                 print()
                 print('*****Restaurant item manage*****')
@@ -110,20 +112,35 @@ class Manage_Restaurant:
                     # Add item to breakfast menu
                     if staff == 1:
                        
-                                   
-                        self.item_list = {}
-                        self.item_Id = int(input('Enter your item id: '))
-                        self.item_name = input('Enter your item name: ')
-                        self.item_price = int(input('Enter your item price: '))
-                        self.item_category = input('Enter your type (haph ya full): ')
-                        self.item_list['id'] = self.item_Id
-                        self.item_list['name'] = self.item_name
-                        self.item_list['price'] = self.item_price
-                        self.item_list['type'] = self.item_category
-                        # Add to breakfast menu
-                        self.menu_details[0].append(self.item_list) 
-                        print('add successfully!') 
+                        
+                        while True:
+                                           
+                            self.item_list = {}
+                            # self.item_Id = int(input('Enter your item id: '))
+                            self.item_Id=self.Id_Item
+                            self.item_name = input('Enter your item name: ')
+                            
+                            id_exists = False
+                            for item in self.menu_details[0]:
+                                
+                                if item.get('name') == self.item_name:
+                                    
+                                    id_exists = True
+                                    break
 
+                            if  id_exists:
+                                print('This item is available . Not added.')
+                            else:
+                                # self.item_name = input('Enter your item name: ')
+                                self.item_price = int(input('Enter your item price: '))
+                                self.item_category = input('Enter your type (haph ya full): ')
+                                self.item_list['id'] = self.item_Id
+                                self.item_list['name'] = self.item_name
+                                self.item_list['price'] = self.item_price
+                                self.item_list['type'] = self.item_category
+                                self.menu_details[0].append(self.item_list)
+                                print('Item added successfully!')
+                                break
                         # Save updated menu to file
                         with open(self.path, 'w') as file:
                             json.dump(self.menu_details, file, indent=4)
@@ -132,19 +149,32 @@ class Manage_Restaurant:
                     # Add item to lunch menu
                     elif staff == 2:
                         
-                           
-                        self.item_list = {}
-                        self.item_Id = int(input('Enter your item id: '))
-                        self.item_name = input('Enter your item name: ')
-                        self.item_price = int(input('Enter your item price: '))
-                        self.item_category = input('Enter your type (haph ya full): ')
-                        self.item_list['id'] = self.item_Id
-                        self.item_list['name'] = self.item_name
-                        self.item_list['price'] = self.item_price
-                        self.item_list['type'] = self.item_category  
-                    
-                        self.menu_details[1].append(self.item_list) 
-                        print('add successfully!') 
+                        while True: 
+                            self.item_list = {}  
+                            self.item_Id=self.Id_Item
+                            self.item_name = input('Enter your item name: ')
+                            
+                            id_exists = False
+                            for item in self.menu_details[1]:
+                                
+                                if item.get('name') == self.item_name:
+                                    
+                                    id_exists = True
+                                    break
+
+                            if  id_exists:
+                                print('This item is available. Not added.')
+                            else:
+                                # self.item_name = input('Enter your item name: ')
+                                self.item_price = int(input('Enter your item price: '))
+                                self.item_category = input('Enter your type (haph ya full): ')
+                                self.item_list['id'] = self.item_Id
+                                self.item_list['name'] = self.item_name
+                                self.item_list['price'] = self.item_price
+                                self.item_list['type'] = self.item_category
+                                self.menu_details[1].append(self.item_list)
+                                print('Item added successfully!')
+                                break
 
                         # Save updated menu to file
                         with open(self.path, 'w') as file:
@@ -155,7 +185,7 @@ class Manage_Restaurant:
                         
                         while True:
                             
-                            self.Id_number =int(input('Enter item Id number to remove: '))
+                            self.Id_number =input('Enter item Id number to remove: ')
                             found=False
                             for item in self.menu_details[0]:
                                 for key,value in item.items():
@@ -179,7 +209,7 @@ class Manage_Restaurant:
                     elif staff == 4:
                         
                         while True:
-                            self.Id_number =int(input('Enter item Id number to remove: '))
+                            self.Id_number =input('Enter item Id number to remove: ')
                             found=False
                             for item in self.menu_details[1]:
                                 for key,value in item.items():
